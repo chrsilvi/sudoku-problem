fda2d295765413277d45be15bf2ecf81d700bde4
Chris Silvia <chrisdsilvia@gmail.com>
2020-08-21 21:04:14 -0400

    Iteratively goes through every number combination
    Figured out a recursive function in order to go through every
    combination of digits (I'm pretty sure?), so now it's a matter
    of figuring out how to compare that to the sum I'm looking for and
    adding to a dictionary. But it seems like so much work for the computer
    to go through every combination 45 times (45 being the largest sum
    you can reach with nine digits).
    
    Having just written that I think the goal would be to have a series of
    sums in a dictionary, and each time I go through add that combination
    to that sum to only go through every iteration once instead of 45 times.

--- sudoku.py
+++ sudoku.py
@@ -3,37 +3,46 @@ combinations = {}
 # combinations[1] = [1]
 # combinations[3] = [2, 1]
 # combinations[5] = [[4, 1], [3, 2]]
-digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+digits = [1, 2, 3, 4]
 
 # this goes through up to 3 digits, trying to figure out how to formulate
 # this to go through anywhere from 1-9 digits
 # need to use recursion somehow in the bowels of this function
 
 variable_list = []
-current_sum = 1
-def variables():
-    global current_sum
-    global digits
-    while current_sum < 46:
-        for x in range(len(digits)):
-            variable_list.append(digits[x])
-            digits.remove(digits[x])
-            verify_combination(variable_list, current_sum)
-            print(current_sum)
-            print(variable_list)
-            current_sum += 1
-            for y in range(len(digits)):
-                variable_list.append(digits[y])
-                print(variable_list)
-                variable_list.remove(digits[y])
-            digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            variable_list.remove(digits[x])
+current_sum = 3
 
-    #cycle through and provide every combination of variables
 
 def verify_combination(variable_list, current_sum):
-    pass
-    #double check if that combination of variables equals the sum
-    #adds it to combinations if so
+    this_total = 0
+    for x in range(len(variable_list)):
+        this_total += variable_list[x]
+    if this_total == current_sum:
+        combinations[current_sum] = variable_list
 
-variables();
+
+def inception(digits, current_sum):
+    global variable_list
+    for x in range(len(digits)):
+        this_iteration = digits[x]
+        this_iteration = digits[x]
+        variable_list.append(this_iteration)
+        digits.remove(this_iteration)
+        print(variable_list)
+        verify_combination(variable_list, current_sum)
+        inception(digits, current_sum)
+        variable_list.remove(this_iteration)
+        digits.append(this_iteration)
+        digits.sort()
+
+
+def variables():
+    global current_sum
+    global master_digits
+    global digits
+    print(current_sum)
+    inception(digits, current_sum)
+
+
+variables()
+print(combinations)
